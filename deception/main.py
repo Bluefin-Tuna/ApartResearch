504295dad6a36da8c7a47c1c2026be5e92bbb0e7
Tanush Chopra <tchopra32@gatech.edu>
2024-09-10 12:59:19 -0700

    feat: added few shot experiments (api)

--- deception/main.py
+++ deception/main.py
@@ -198,8 +198,11 @@ if __name__ == "__main__":
 
     run_control_experiment(NUM_GAMES, "baseline")
 
-    thread1 = threading.Thread(target=run_agent_experiment, args=(NUM_GAMES, "gpt_0.0_zero_shot", agent_gpt_0, ZERO_SHOT_PROMPT))
-    thread2 = threading.Thread(target=run_agent_experiment, args=(NUM_GAMES, "claude_0.0_zero_shot", agent_claude_0, ZERO_SHOT_PROMPT))
+    thread1 = threading.Thread(target=run_agent_experiment, args=(NUM_GAMES, "gpt_0.0_few_shot", agent_gpt_0, FEW_SHOT_PROMPT))
+    thread2 = threading.Thread(target=run_agent_experiment, args=(NUM_GAMES, "claude_0.0_few_shot", agent_claude_0, FEW_SHOT_PROMPT))
+
+    thread3 = threading.Thread(target=run_agent_experiment, args=(NUM_GAMES, "gpt_0.5_few_shot", agent_gpt_5, FEW_SHOT_PROMPT))
+    thread4 = threading.Thread(target=run_agent_experiment, args=(NUM_GAMES, "claude_0.5_few_shot", agent_claude_5, FEW_SHOT_PROMPT))
 
     # thread2 = threading.Thread(target=run_agent_experiment, args=(claude, IMPLICIT_SYSTEM_PROMPT, "claude_implicit", NUM_GAMES))
     # thread3 = threading.Thread(target=run_agent_experiment, args=(mixstral, IMPLICIT_SYSTEM_PROMPT, "mixstral_implicit", NUM_GAMES))
@@ -208,10 +211,10 @@ if __name__ == "__main__":
     # thread5 = threading.Thread(target=run_agent_experiment, args=(claude, EXPLICIT_SYSTEM_PROMPT, "claude_explicit", NUM_GAMES))
     # thread6 = threading.Thread(target=run_agent_experiment, args=(mixstral, EXPLICIT_SYSTEM_PROMPT, "mixstral_explicit", NUM_GAMES))
 
-    for thread in [thread1, thread2]:
+    for thread in [thread1, thread2, thread3, thread4]:
         thread.start()
     
-    for thread in [thread1, thread2]:
+    for thread in [thread1, thread2, thread3, thread4]:
         thread.join()
 
     # control_files = {
